71ff95779f1e35de2cf9e871d075835b5935763b
Felipe Borges <felumais14@gmail.com>
2020-10-15 15:52:02 -0300

    api security

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../Security/auth.service";
 
 @Component({
   selector: 'app-index',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  authRequest:any={
+    "userName":"salsicha@salsicha.com",
+    "password":"felipe"
+  }
+  response:any
+
+
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+  this.getAccessToken(this.authRequest)
+  }
+
+  public getAccessToken(authR){
+    let resp = this.authService.generateToken(authR)
+    resp.subscribe(data=>this.accessApi(data))
 
   }
+  public accessApi(token){
+    let resp= this.authService.welcome(token)
+    resp.subscribe(data=>this.response = data)
+  }
+
 
 }
